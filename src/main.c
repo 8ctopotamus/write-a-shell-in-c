6e5846f47c5acce71f6a3b1abbc37e6cce562503
8ctopotamus <zylo.codes@gmail.com>
2023-11-26 07:49:33 -0600

    split lines

--- src/main.c
+++ src/main.c
@@ -1,40 +1,55 @@
 #include <stdio.h>
+#include <string.h>
 
 #define RL_BUFSIZE 1024
 char *_read_line(void)
 {
-	int bufsize = RL_BUFSIZE;
-	int position = 0;
-	char *buffer = malloc(sizeof(char) * bufsize);
-	int c;
+	char *line = NULL;
+	ssize_t bufsize = 0; // have getline allocate a buffer for us
 
-	if (!buffer) {
-	 fprintf(stderr, "_: allocation error\n");
-	 exit(EXIT_FAILURE);
+	if (getline(&line, &bufsize, stdin) == -1) {
+		if (feof(stdin)) {
+			exit(EXIT_SUCCESS); // we recieved an EOF
+		} else {
+			perror("readline");
+			exit(EXIT_FAILURE);
+		}
 	}
 
-	while(1) {
-		// read a character
-		c = getchar();
-		// if we hit EOF, replace it with a null characterand return
-		if (c == EOF || c == '\n') {
-			buffer[position] = '\0';
-			return buffer;
-		} else {
-			buffer[position] = c;			
-		}
-		position++;
+	return line;
+}
 
-		// if we have exceeded the buffer, reallocate memory
+#define TOK_BUFSIZE 64
+#define TOK_DELIM " \t\r\n\a"
+char **_split_line(char *line)
+{
+	int bufsize = TOK_BUFSIZE, position = 0;
+	char **tokens = malloc(bufsize * sizeof(char*));
+	char *token;
+
+	if (!tokens) {
+		fprintf(stderr, "allocation error\n");
+		exit(EXIT_FAILURE);
+	}
+
+	token = strtok(line, TOK_DELIM);
+	while (token != NULL) {
+		tokens[position] = token;
+		position++;
+		
 		if (position >= bufsize) {
-			bufsize += RL_BUFFSIZE;
-			buffer = realloc(buffer, buffsize);
-			if (!buffer) {
-				fprintf(stderr, "allocation error\n");
+			bufsize += TOK_BUFSIZE;
+			tokens = realloc(tokens, bufsize * sizeof(char*));
+			if (!tokens) {
+				fprintf(stderr, "allocation err\n");
 				exit(EXIT_FAILURE);
 			}
 		}
+
+		token = strtok(NULL, TOK_DELIM);
 	}
+	tokens[position] = NULL;
+	return tokens;
 }
 
 void _loop(void) {
